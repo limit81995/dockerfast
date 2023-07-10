8a175e26245ef6fa95905e35a4a6dacd0472114a
limit <limit@gszzz.com>
2023-07-10 18:15:54 +0800

    优化setup文件，更新安装方式方便单独安装，修复不能从任意路径安装的问题

---
+++ setup/mysql/config.sh
@@ -0,0 +1,10 @@
+#### MYSQL配置
+
+#容器名称
+MYSQL_CONTAINER_NAME="mysql_latest"
+#ROOT密码
+MYSQL_ROOT_PASSWORD="123456"
+#本地数据映射目录
+MYSQL_DATA_DIR="/../../contains/mysql/data"
+#对外端口
+MYSQL_PORT=3306
--- setup/mysql/setup.sh
+++ setup/mysql/setup.sh
@@ -1,5 +1,18 @@
-source ../config.sh
+#### 安装脚本
+
+# 当前脚本路径
+CURRENT_DIR=$(cd $(dirname $0);pwd)
+
+source ${CURRENT_DIR}/config.sh
+
 docker build .
-echo "正在安装MYSQL...(INSTALLING MYSQL...)"
-docker run --name ${MYSQL_CONTAINER_NAME} -v ${MYSQL_DATA_DIR}:/var/lib/mysql -p ${MYSQL_PORT}:3306 -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} -d  mysql:latest
+echo "INSTALLING MYSQL ..."
+
+docker run \
+--name ${MYSQL_CONTAINER_NAME} \
+-v ${CURRENT_DIR}${MYSQL_DATA_DIR}:/var/lib/mysql \
+-p ${MYSQL_PORT}:3306 \
+-e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+-d  mysql:latest
+
 # 容器地址：   数据库配置地址 /etc/mysql/conf.d/mysql.cnf 数据库地址：/var/lib/mysql
