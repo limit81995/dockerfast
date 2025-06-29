59ced3c47ade21489a053baf7750169b721f4df7
limit <81995484@qq.com>
2025-06-29 23:51:43 +0800

    feat：nginx_php74

---
+++ setup/nginx_php74/setup.sh
@@ -0,0 +1,51 @@
+################################################
+
+################## 容器配置
+
+# 当前脚本路径
+SETUP_CURRENT_DIR=$(cd $(dirname $0);pwd)
+#APP 名称
+APP_NAME=${SETUP_CURRENT_DIR##*/};
+# 镜像名称
+IMAGE_NAME="webdevops/php-nginx"
+# 镜像版本号
+IMAGE_VERSION="7.4"
+# 端口
+APP_PORT="80"
+# 容器名称
+APP_CONTAINER_NAME="${APP_NAME}"
+# APP通用安装目录地址
+CONTAINERS_APP_DIR=${SETUP_CURRENT_DIR}/../../containers/${APP_NAME}
+
+# 检查容器目录是否存在 不存在则创建
+if [ ! -d ${SETUP_CURRENT_DIR}/../../containers ]; then
+  mkdir ${SETUP_CURRENT_DIR}/../../containers
+fi
+
+if [ ! -d ${CONTAINERS_APP_DIR} ]; then
+  mkdir ${CONTAINERS_APP_DIR}
+fi
+
+if [ ! -d ${CONTAINERS_APP_DIR}/conf ]; then
+  mkdir ${CONTAINERS_APP_DIR}/conf
+fi
+
+# 如果containers中没有配置文件，则复制默认配置文件
+
+if !(test -f "${CONTAINERS_APP_DIR}/conf/nginx.conf"); then
+  cp ${SETUP_CURRENT_DIR}/temp/nginx.conf ${CONTAINERS_APP_DIR}/conf/nginx.conf
+fi
+
+############ 安装脚本
+
+docker run \
+--name ${APP_CONTAINER_NAME} \
+-v ${CONTAINERS_APP_DIR}/data:/var/www/html \
+-v ${CONTAINERS_APP_DIR}/conf/nginx.conf:/etc/nginx/nginx.conf \
+-v ${CONTAINERS_APP_DIR}/conf/conf.d:/etc/nginx/conf.d \
+-v ${CONTAINERS_APP_DIR}/cert:/etc/nginx/cert \
+-p ${APP_PORT}:80 \
+-p 443:443 \
+-d ${IMAGE_NAME}:${IMAGE_VERSION}
+
+###############################################
