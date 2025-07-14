04819067c33a764e7c754274bd836455aec35f12
limit <81995484@qq.com>
2025-07-14 11:24:09 +0800

    feat：增加kafka和kafka-ui功能

---
+++ app/kafka/setup.sh
@@ -0,0 +1,25 @@
+# 当前脚本路径
+SETUP_CURRENT_DIR=$(cd $(dirname $0);pwd)
+#APP 名称
+APP_NAME=${SETUP_CURRENT_DIR##*/};
+# APP通用安装目录地址
+CONTAINERS_APP_DIR=${SETUP_CURRENT_DIR}/../../containers/${APP_NAME}
+
+# 检查容器目录是否存在 不存在则创建
+if [ ! -d ${SETUP_CURRENT_DIR}/../../containers ]; then
+  mkdir ${SETUP_CURRENT_DIR}/../../containers
+fi
+
+if [ ! -d ${CONTAINERS_APP_DIR} ]; then
+  mkdir ${CONTAINERS_APP_DIR}
+fi
+
+#暴露的环境变量
+export APP_NAME=${APP_NAME}
+export CONTAINERS_APP_DIR=${CONTAINERS_APP_DIR}
+
+sudo chown -R 1001:1001 ${CONTAINERS_APP_DIR}
+sudo chmod -R 777 ${CONTAINERS_APP_DIR}
+# 创建共用网络
+docker network create --driver bridge local_network || true
+docker-compose up -d
