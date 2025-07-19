41d1b380548144c23498ba78bd6175590c47b959
limit <81995484@qq.com>
2025-07-20 01:05:59 +0800

    feat：升级mysql

--- app/mysql/setup.sh
+++ app/mysql/setup.sh
@@ -1,21 +1,7 @@
-################################################
-
-################## 容器配置
-
 # 当前脚本路径
 SETUP_CURRENT_DIR=$(cd $(dirname $0);pwd)
 #APP 名称
 APP_NAME=${SETUP_CURRENT_DIR##*/};
-#root 初始化密码
-APP_ROOT_PASSWORD=123456
-# 镜像名称
-IMAGE_NAME="mysql"
-# 镜像版本号
-IMAGE_VERSION="8.0.30"
-# 端口
-APP_PORT="3306"
-# 容器名称
-APP_CONTAINER_NAME="${APP_NAME}"
 # APP通用安装目录地址
 CONTAINERS_APP_DIR=${SETUP_CURRENT_DIR}/../../containers/${APP_NAME}
 
@@ -28,31 +14,18 @@ if [ ! -d ${CONTAINERS_APP_DIR} ]; then
   mkdir ${CONTAINERS_APP_DIR}
 fi
 
-if [ ! -d ${CONTAINERS_APP_DIR}/data ]; then
-  mkdir ${CONTAINERS_APP_DIR}/data
-fi
-
 if [ ! -d ${CONTAINERS_APP_DIR}/conf ]; then
   mkdir ${CONTAINERS_APP_DIR}/conf
+  cat ./my.cnf > ${CONTAINERS_APP_DIR}/conf/my.cnf
 fi
 
-# 如果containers中没有配置文件，则复制默认配置文件
+#暴露的环境变量
+export APP_NAME=${APP_NAME}
+export CONTAINERS_APP_DIR=${CONTAINERS_APP_DIR}
 
- if !(test -f "${CONTAINERS_APP_DIR}/conf/my.cnf"); then
-   cp ${SETUP_CURRENT_DIR}/temp/my.cnf ${CONTAINERS_APP_DIR}/conf/my.cnf
- fi
-
-############ 安装脚本
-
-docker run \
---name ${APP_CONTAINER_NAME} \
--v ${CONTAINERS_APP_DIR}/data:/var/lib/mysql \
--v ${CONTAINERS_APP_DIR}/conf:/etc/mysql/conf.d \
--e MYSQL_ROOT_PASSWORD=123456 \
--p ${APP_PORT}:3306 \
--d ${IMAGE_NAME}:${IMAGE_VERSION}
-
-
+# 创建共用网络
+docker network create --driver bridge local_network || true
+docker-compose up -d
 
 ######### 注意：安装后需要将 mysql user表中的root用户的host 改为"%";用来所有地址都可以访问。
 ## 连接mysql
