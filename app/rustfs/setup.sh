35ea9b34e71bbad39308ff3371bb48c0895d416e
limit <limit@limit.com>
2026-04-20 14:56:55 +0800

    add:由于minio转商业化，删除minio包。更换为rustfs开源项目

--- app/rustfs/setup.sh
+++ app/rustfs/setup.sh
@@ -5,14 +5,11 @@ APP_NAME=${SETUP_CURRENT_DIR##*/};
 # APP通用安装目录地址
 CONTAINERS_APP_DIR=${SETUP_CURRENT_DIR}/../../containers/${APP_NAME}
 
-# 检查容器目录是否存在 不存在则创建
-if [ ! -d ${SETUP_CURRENT_DIR}/../../containers ]; then
-  mkdir ${SETUP_CURRENT_DIR}/../../containers
-fi
-
-if [ ! -d ${CONTAINERS_APP_DIR} ]; then
-  mkdir ${CONTAINERS_APP_DIR}
-fi
+mkdir -p ${SETUP_CURRENT_DIR}/../../containers
+mkdir -p ${CONTAINERS_APP_DIR}/data
+mkdir -p ${CONTAINERS_APP_DIR}/logs
+  
+docker run --rm -v "${CONTAINERS_APP_DIR}:/target" alpine chown -R 10001:10001 /target
 
 #暴露的环境变量
 export APP_NAME=${APP_NAME}
