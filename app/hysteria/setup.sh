86983c9258493457fb229edc9ff826b865aed741
limit <limit@limit.com>
2026-05-07 21:24:00 +0800

    fix:调整项目脚本，将配置项抽离到.env中。

--- app/hysteria/setup.sh
+++ app/hysteria/setup.sh
@@ -1,3 +1,8 @@
+if [ -f ./.env ]; then
+  set -a
+  . ./.env
+  set +a
+fi
 # 当前脚本路径
 SETUP_CURRENT_DIR=$(cd $(dirname $0);pwd)
 #APP 名称
@@ -10,9 +15,29 @@ mkdir -p ${CONTAINERS_APP_DIR}
 export APP_NAME=${APP_NAME}
 export CONTAINERS_APP_DIR=${CONTAINERS_APP_DIR}
 
-if [ ! -f "${CONTAINERS_APP_DIR}/config.yaml" ]; then
-    cat ./config.yaml > ${CONTAINERS_APP_DIR}/config.yaml
-fi
+NODE_IP=$(curl -4s https://api.ipify.org)
+
+cat > ${CONTAINERS_APP_DIR}/config.yaml <<EOF
+listen: :${APP_PORT}
+trafficStats:
+  listen: :${API_PORT}
+
+tls:
+  cert: /etc/server.crt
+  key: /etc/server.key
+auth:
+  type: ${AUTH_TYPE}
+  password: ${AUTH_PASSWORD}
+  http:
+    url: ${AUTH_HTTP_URL}?ip=${NODE_IP}
+ignoreClientBandwidth: false
+masquerade:
+  type: proxy
+  proxy:
+    url: https://www.bing.com
+    rewriteHost: true
+EOF
+
 cat ./server.crt > ${CONTAINERS_APP_DIR}/server.crt
 cat ./server.key > ${CONTAINERS_APP_DIR}/server.key
 
