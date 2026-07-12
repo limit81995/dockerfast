e0f5c5a4ee19c0421ac412321a3304574784a708
limit <limit@limit.com>
2026-07-12 22:26:28 +0800

    feat：增加chatwoot

---
+++ app/chatwoot/setup.sh
@@ -0,0 +1,42 @@
+
+## 加载.env 配置文件到脚本中
+if [ -f ./.env ]; then
+  set -a
+  . ./.env
+  set +a
+fi
+
+# 当前脚本路径
+SETUP_CURRENT_DIR=$(cd $(dirname $0);pwd)
+#APP 名称
+APP_NAME=${SETUP_CURRENT_DIR##*/};
+# APP通用安装目录地址
+CONTAINERS_APP_DIR=${SETUP_CURRENT_DIR}/../../containers/${APP_NAME}
+
+mkdir -p ${CONTAINERS_APP_DIR}
+
+#暴露的环境变量
+export APP_NAME=${APP_NAME}
+export CONTAINERS_APP_DIR=${CONTAINERS_APP_DIR}
+
+########  以上为通用配置无需调整  ########
+
+
+
+########  以下是自定义内容  ########
+
+# 根据模板生成配置文件 
+envsubst < .env.chatwoot > ${CONTAINERS_APP_DIR}/.env
+
+
+# 创建共用网络
+docker network create --driver bridge local_network || true
+
+# 启动容器
+docker compose down && docker compose up -d
+
+read -r -p "是否初始化/更新 Chatwoot 数据库？[y/N]: " confirm_db
+if [[ "${confirm_db}" =~ ^[Yy]$ ]]; then
+# 初始化db文件
+  docker compose run --rm rails bundle exec rails db:chatwoot_prepare
+fi
